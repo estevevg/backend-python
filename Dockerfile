eec42d72c477037ad9b47ef9599bcd77b6e7d543
Esteve Verdura Gelma <esteve.verdura@gmail.com>
2018-06-12 21:21:02 +0200

    Adds docker-compose

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,5 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
+ENTRYPOINT ["./entrypoint.sh"]
 CMD ["python", "run.py"]
--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ MONGO_HOST = 'localhost'
 MONGO_PORT = 27017
 
 # Skip these if your db has no auth. But it really should.
-MONGO_USERNAME = '<your username>'
-MONGO_PASSWORD = '<your password>'
+MONGO_USERNAME = '<your_username>'
+MONGO_PASSWORD = '<your_password>'
 
 MONGO_DBNAME = 'apitest'

53b8c8afcd66f08f75d252b9443b2aaeda6d3b93
e.verdura <e.verdura@icgc.cat>
2018-06-14 16:31:16 +0200

    Changes the entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 sed -i s/localhost/$DB_HOST/ settings.py
 sed -i s/27017/$DB_PORT/ settings.py
-sed -i s/<your_username>/$DB_USER/ settings.py
-sed -i s/<your_password>/$DB_PASSWORD/ settings.py
+sed -i s/your_username/$DB_USER/ settings.py
+sed -i s/your_password/$DB_PASSWORD/ settings.py
 sed -i s/apitest/$DB_NAME/ settings.py
 
 exec '$@'

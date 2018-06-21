713ace0e12325289a6c3be00d19617c356fd5bdd
e.verdura <e.verdura@icgc.cat>
2018-06-21 12:04:12 +0200

    Authentication for mongo is out

--- settings.py
+++ settings.py
@@ -1,14 +1,10 @@
 DOMAIN = {'people': {}}
 
-# Let's just use the local mongod instance. Edit as needed.
-
-# Please note that MONGO_HOST and MONGO_PORT could very well be left
-# out as they already default to a bare bones local 'mongod' instance.
 MONGO_HOST = 'localhost'
 MONGO_PORT = 27017
 
-# Skip these if your db has no auth. But it really should.
-MONGO_USERNAME = 'your_username'
-MONGO_PASSWORD = 'your_password'
+#MONGO_USERNAME = 'root'
+#MONGO_PASSWORD = 'example'
 
-MONGO_DBNAME = 'apitest'
+#MONGO_DBNAME = 'apitest'
+RESOURCE_METHODS = ['GET']

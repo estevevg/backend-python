f8f0b81f0c8586375f474829b2be1477deba7c9b
e.verdura <e.verdura@icgc.cat>
2018-06-27 15:38:54 +0200

    Adds DB schema

--- settings.py
+++ settings.py
@@ -1,10 +1,15 @@
-DOMAIN = {'people': {}}
+import dbschema
 
-MONGO_HOST = 'localhost'
+DOMAIN = {'user': dbschema.user}
+
+MONGO_HOST = '172.17.50.127'
 MONGO_PORT = 27017
 
 #MONGO_USERNAME = 'root'
 #MONGO_PASSWORD = 'example'
 
 #MONGO_DBNAME = 'apitest'
-RESOURCE_METHODS = ['GET']
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+
+
